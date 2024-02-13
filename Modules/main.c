b775bd301b3960987f8b0fc142eb1ec68a0c6c81
Samara <sferreira@ecomp.uefs.br>
2024-02-12 23:12:12 -0300

    ✨ feat(main): adição do código principal do jogo.

---
+++ Modules/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <constants.h>
+//#include <intelfpgaup/accel.h>
+#include <blocks.c>
+#include <ball.c>
+#include <bar.c>
+
+
+int checkWin(Block blocksList[]) {
+    for (int i = 0; i < QUANTITY_BLOCKS; i++) {
+        if ((blocksList[i].flagVisible == 1) && (blocksList[i].life != -1))
+            return 1;
+    }
+
+    return 0;
+}
+
+int checkLose(Ball *ball) {
+    if (ball->ballPositionY < (SCREEN_Y - 20))
+        return 0;
+    else 
+        return 1;
+}
+
+
+int main() {
+
+    /* Inicializar os elementos do jogo */
+
+    Block blocksList[QUANTITY_BLOCKS];
+    /* em andamento... */
+
+    Ball ball;
+    ball.ballPositionX = SCREEN_X / 2;
+    ball.ballPositionY = SCREEN_Y - 60;
+    ball.ballSpeedX = 2;
+    ball.ballSpeedY = 5;
+    
+    Bar bar;
+    bar.coordX = SCREEN_X / 2;
+    bar.coordY = SCREEN_Y - 20;
+
+    int score = 0;
+
+
+    /* Inicializações */
+
+    int ready, tap, dtap, velX, velY, velZ, mg_per_lsb;
+
+    accel_open();
+
+    accel_init();
+
+    accel_calibrate();
+
+
+    /* Loop principal do jogo */
+    while (checkWin(blocksList) && checkLose(&ball)) {
+
+        accel_read(&ready, &tap, &dtap, &velX, &velY, &velZ, &mg_per_lsb);
+
+        /* Movimentar a barra */
+        moveBar(&bar, velX);
+
+        /* Movimentar a bola */
+        moveBall(&ball, blocksList, &bar, &score);        
+    }
+
+
+    return 0;
+}
