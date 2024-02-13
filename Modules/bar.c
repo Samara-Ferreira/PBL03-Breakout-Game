87014e9f0e427255dfada31e1622c586011b33db
Samara <sferreira@ecomp.uefs.br>
2024-02-12 23:10:11 -0300

    ✨ feat(bar): adição da biblioteca relacionada a barra.

---
+++ Modules/bar.c
@@ -0,0 +1,20 @@
+//#include <intelfpgaup/accel.h>
+#include <constants.h>
+
+typedef struct {
+    int coordX;
+    int coordY; 
+} Bar;
+
+/* Função para movimentar a barra */
+void moveBar(Bar *bar, int accelX) {
+    if ((WALL_WIDHT_X < (bar->coordX - BAR_SIZE) + accelX) && ((bar->coordX + BAR_SIZE) + accelX < SCREEN_X - WALL_WIDHT_X))
+        bar->coordX += accelX;
+    else {
+        if (SCREEN_X / 2 >= bar->coordX) 
+            bar->coordX -= bar->coordX - BAR_SIZE - WALL_WIDHT_X;
+        else 
+            bar->coordX += -(bar->coordX + BAR_SIZE) + SCREEN_X - WALL_WIDHT_X;
+    }
+}
+
