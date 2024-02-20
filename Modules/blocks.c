e312e0f7d845eb64fbcf29941c7851a8ec9679f6
Samara <sferreira@ecomp.uefs.br>
2024-02-19 22:07:44 -0300

    ♻️ refactor(blocks): adição de nova função e modificação na referente a remoção dos blocos.

--- Modules/blocks.c
+++ Modules/blocks.c
@@ -1,50 +1,34 @@
-#include "constants.h"
-#include "blocks.c"
-#include "ball.c"
+#include "prototype.h"
 
-typedef struct {
-    int coordX;         /* x do canto superior esquerdo */ 
-    int coordY;         /* y do canto superior esquerdo */
-    int flagVisible;    /* flag para saber se o bloco esta visivel */
-    int life;           /* vida do bloco */ 
-    int points;         /* pontos do bloco */
-} Block;
+void makeBlocks (Block blocksList[]) {
 
+	int i, j, count = 0;
+	short colors[6] = {0xf012, 0xe800, 0xf301, 0xdd82, 0x5ec, 0xf37}; //rosa forte, vermelho escuro, laranja, amarelo dourado, ciano claro, verde vivo
 
-int detect_collision(int x_rect, int y_rect, int length_rect, int width_rect,  int x_boll, int y_boll, int ray, int *x_point, 
-int *y_point) {
+	for (i = 0; i < 6; i++) { 
+	
+	    for (j = 0; j < 8; j++) {
+	    
+		blocksList[count].coordX =  5 + (39 * j);
+		blocksList[count].coordY = 20 + (i * 14);
+		blocksList[count].flagVisible = 1; 
+		blocksList[count].life   =  1;
+		blocksList[count].points =  11 - (i * 2); 
+		blocksList[count].color  =  colors[i];
 
-    *x_point = x_boll;  /* Ponto de impacto em x */
-    *y_point = y_boll;  /* ponto de impacto em y */
-    
-    /* Procurar o ponto mais proximo do circulo no eixo x */
-    if (x_boll < x_rect)
-        (*x_point) = x_rect;
-    else if (x_boll > x_rect + length_rect) 
-        (*x_point) = x_rect + length_rect;
-
-    /* Procurar o ponto mais proximo do circulo no eixo y */
-    if (y_boll < y_rect)
-        (*y_point) = y_rect;
-    else if (y_boll > y_rect + width_rect) 
-        (*y_point) = y_rect + width_rect;
-
-    /* Calcular distancia e verificar se a bola esta dentro bloco */
-    int dist_x = x_boll - *x_point;
-    int dist_y = y_boll - *y_point;
-    int distance = round(sqrt(dist_x * dist_x + dist_y * dist_y));
-
-    if (distance <= ray)
-        return 1;
-
-    return 0;
+		count++;  
+        }  
+	}  
+	
 }
 
-
 /* Função para remover os blocos */
-void removeBlocks(Block blocksList[], int *score,  Ball *ball) {
+void removeBlocks (Block blocksList[], int *score,  Ball *ball) {
+
+    int x_point, y_point;
 
     for (int i = 0; i < QUANTITY_BLOCKS; i++) {
+     
 
         if ((detect_collision(blocksList[i].coordX, blocksList[i].coordY, BLOCK_LENGTH, BLOCK_WIDHT, ball->ballPositionX, ball->ballPositionY, 
         COLLISION_RADIUS, &x_point, &y_point) == 1) && (blocksList[i].flagVisible == 1)) {
@@ -59,4 +43,3 @@ void removeBlocks(Block blocksList[], int *score,  Ball *ball) {
         }
     }
 }
-
