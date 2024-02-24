5fb0aa9ebc9f90d61f7f9612e614a523a5ff9758
Silviozv <109181824+Silviozv@users.noreply.github.com>
2024-02-24 15:39:03 -0300

    ✨ feat e docs(constants): Comentando as funções e colocando as constantes das cores.

--- Modules/layout.c
+++ Modules/layout.c
@@ -1,5 +1,9 @@
-#include "prototype.h"
+/**
+ *  Funções de lógica da exibição de dados no monitor.
+ */
 
+
+#include "prototype.h"
 
 /**
  * Desenha uma frase na tela.
@@ -16,7 +20,7 @@
  * @param cor A cor dos caracteres da frase.
  * @return void
  */
-void phrase(int coordX, int coordY, char *list, int lenList, short cor) {
+void phrase(int coordX, int coordY, char *list, int lenList, short color) {
 
     int X;
 
@@ -25,15 +29,17 @@ void phrase(int coordX, int coordY, char *list, int lenList, short cor) {
         if (list[i] != ' ') {
 
             X = coordX + i * 13;
-            alphanumeric(X, coordY, list[i], cor);
+            alphanumeric(X, coordY, list[i], color);
 
         }
     }
 }
 
 /**
+ * Exibição do valor do score.
+ * 
  * Converte o valor do score do tipo inteiro para um vetor do tipo char. Cada caractere do vetor
- * é percorrido e enviado para a função que irá exibir cada um dos valores através do cabo VGA.
+ * é percorrido e enviado para a função que irá exibir cada um dos valores.
  * 
  * @param coordX A coordenada X do canto superior esquerdo da região onde o valor será exibido.
  * @param coordY A coordenada Y do canto superior esquerdo da região onde o valor será exibido.
@@ -51,13 +57,16 @@ void write_score( int coordX, int coordY, int score) {
 
     for (i = 0; i < len; i++) {
 
-        alphanumeric(coordX + (13 * i), coordY, number_str[i], 0xFFE0);
+        alphanumeric(coordX + (13 * i), coordY, number_str[i], COLOR_YELLOW);
     }
 }
 
 /**
- * Exibe a tela de derrota. São exibidas as frases "game over" e o valor do score alcançado, utilizando
- * as funções de escrita de frase e exibição do valor do score.
+ * Exibe a tela de derrota. 
+ * 
+ * São exibidas as frases "game over" e o valor do score alcançado, utilizando as funções 
+ * de escrita de frase e exibição do valor do score. Dependendo de quantos algorismos o 
+ * score possui, a coordenada inicial de X é alterada.
  * 
  * @param score Valor do score alcançado na rodada.
  * @return void
@@ -77,33 +86,38 @@ void screen_defeat (int score) {
         sub_coordX = 5;
     }
 
-    phrase(100, 110, defeat_message, 9, 0xf800);
-    phrase(110 - sub_coordX, 145, score_message, 6, 0xffe0);
+    phrase(100, 110, defeat_message, 9, COLOR_RED);
+    phrase(110 - sub_coordX, 145, score_message, 6, COLOR_YELLOW);
     write_score(188 - sub_coordX,  145, score);
 
 }
 
+/**
+ * Exibe a tela de vitória. 
+ * 
+ * É exibida a frase "you win" e uma animação constante de fogos. Primeiramente, são liberados 
+ * dois foguetes que percorrem verticalmente a região da tela e depois são armazenadas as suas 
+ * imagens de explosão. Após isso, mais foguetes percorrem a tela, um após o outro, com coordenadas 
+ * de X e Y variadas. Suas explosões também são exibidas e armazenadas. Só podem ser exibidas, ao todo, 
+ * 7 imagens de explosões, retirando as mais antigas. O loop termina quando o retorno dos sinais dos 
+ * botões retorna que houve algum clique. Com seu término, todas as explosões são liberadas da memória. 
+ * 
+ * @return void
+ */
 void screen_victory () {
 
     char victory_message[7] = "you win";
 
-    int red = 0xF800;
-    int blue = 0x041F;
-    int yellow = 0xFFE0;
-    int white = 0xFFFF;
-    int green = 0x07E0;
-    int pink = 0xFC18;
-    int orange = 0xFC00;
-    int listColors[] = {yellow, green, pink, orange, red, blue};
+    int listColors[] = { COLOR_YELLOW, COLOR_GREEN, COLOR_PINK, COLOR_ORANGE, COLOR_RED, COLOR_BLUE, COLOR_MAGENTA};
     int i, j, indexColor = 0;
 
     for ( i = 214; i > 37; i--) {
 
         video_clear();
 
-        rocket( 38, i, white, red);
-        rocket( 258, i, white, blue);
-        phrase(115, 110, victory_message, 7, 0x07e0);
+        rocket( 38, i, COLOR_WHITE, COLOR_RED);
+        rocket( 258, i, COLOR_WHITE, COLOR_BLUE);
+        phrase(115, 110, victory_message, 7, COLOR_GREEN);
 
         video_show();
 
@@ -111,9 +125,9 @@ void screen_victory () {
 
     video_clear();
 
-    explosion (0, 0, red, yellow );
-    explosion (218, 0, blue, yellow );
-    phrase(115, 110, victory_message, 7, 0x07e0);
+    explosion (0, 0, COLOR_RED, COLOR_YELLOW);
+    explosion (218, 0, COLOR_BLUE, COLOR_YELLOW);
+    phrase(115, 110, victory_message, 7, COLOR_GREEN);
 
     video_show();
 
@@ -122,12 +136,12 @@ void screen_victory () {
 
     ex1.coordX = 0;
     ex1.coordY = 0;
-    ex1.color = red;
+    ex1.color = COLOR_RED;
     ex1.prox = &ex2;
 
     ex2.coordX = 218;
     ex2.coordY = 0;
-    ex2.color = blue;
+    ex2.color = COLOR_BLUE;
     ex2.prox = NULL;
 
     List list;
@@ -153,7 +167,7 @@ void screen_victory () {
 
             if (i < 7) {
 
-                explosion ( aux->coordX, aux->coordY, aux->color, yellow);
+                explosion ( aux->coordX, aux->coordY, aux->color, COLOR_YELLOW);
                 aux = aux->prox;
 
             }
@@ -176,17 +190,17 @@ void screen_victory () {
             indexColor++;
 
             if ( coordX < 38) coordX += 38;
-            if ( indexColor > 5) indexColor = 0;
+            if ( indexColor > 6) indexColor = 0;
             
         } 
         else {
 
-            rocket( coordX, j, white, listColors[indexColor]);
+            rocket( coordX, j, COLOR_WHITE, listColors[indexColor]);
             j -= 1;
 
         }
 
-        phrase(115, 110, victory_message, 7, 0x07e0);
+        phrase(115, 110, victory_message, 7, COLOR_GREEN);
         video_show();
 
         KEY_read(&buttons);
@@ -205,8 +219,10 @@ void screen_victory () {
 }
 
 /**
- * Exibe a tela inicial do jogo. É exibido o título do jogo e a frase "start game", utilizando a função de 
- * exibição do título e de exibição da frase.
+ * Exibe a tela inicial do jogo
+ * 
+ * É exibido o título do jogo e a frase "start game", utilizando a função de exibição do título 
+ * e de exibição de frase.
  * 
  * @return void
  */
@@ -214,13 +230,18 @@ void create_menu () {
 
     char start_game[] = "start game";
 
-    phrase(96, 160, start_game, 10, 0xdd82);
+    phrase(96, 160, start_game, 10, 0xDD82);
 
     title(40, 30);
 }
 
 /**
- * Exibe o campo de jogo. É exibido o título do jogo e a frase "start game".
+ * Exibe o campo de jogo. 
+ * 
+ * São mostradas: as linhas que delimitam a área de jogo; todos os blocos não destruídos; 
+ * a bola e a barra, nas suas determinadas posições; o valor do score; e a mensagem indicando 
+ * se o jogo está pausado e se a opção a ser selecionada no momento é de continuar o jogo ou 
+ * voltar para a tela inicial.
  * 
  * @return void
  */
@@ -228,31 +249,31 @@ void game_field(Block blocksList[], int score, int state_game) {
 
     char text_score[6] = "score:";
 
-    phrase(0, 0, text_score, 6, 0xFFE0);
+    phrase(0, 0, text_score, 6, COLOR_YELLOW);
     write_score(78, 0, score);
 
     if (state_game == 1) {
 
         char text_pause[11] = "";
-        phrase(177, 0, text_pause, 11, 0xFFFF);
+        phrase(177, 0, text_pause, 11, COLOR_WHITE);
         
     }
     else if (state_game == 2) {
 
         char text_pause[11] = "pause<play>";
-        phrase(177, 0, text_pause, 11, 0xFFFF);
+        phrase(177, 0, text_pause, 11, COLOR_WHITE);
         
     }
     else if (state_game == 3) {
 
         char text_pause[11] = "pause<exit>";
-        phrase(177, 0, text_pause, 11, 0xFFFF);
+        phrase(177, 0, text_pause, 11, COLOR_WHITE);
         
     }
 
-    video_box(0, 15, 2, 239, 0xC618);
-    video_box(3, 15, 319, 17, 0xC618);
-    video_box(317, 18, 319, 239, 0xC618);
+    video_box(0, 15, 2, 239, COLOR_GREY);
+    video_box(3, 15, 319, 17, COLOR_GREY);
+    video_box(317, 18, 319, 239, COLOR_GREY);
 
     int i;
 
