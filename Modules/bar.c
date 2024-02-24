57927f52b29e9893d08cd7218a74a3faedc96b16
Silviozv <109181824+Silviozv@users.noreply.github.com>
2024-02-24 14:52:16 -0300

    ✨ feat: Consertando posição da barra.

--- Modules/bar.c
+++ Modules/bar.c
@@ -1,12 +1,12 @@
 #include "prototype.h"
 
 void moveBar(Bar *bar, int accelX) {
-    if ((WALL_WIDHT_X < (bar->coordX - BAR_SIZE) + accelX) && ((bar->coordX + BAR_SIZE) + accelX < SCREEN_X - WALL_WIDHT_X ))
+    if ((WALL_WIDHT_X < (bar->coordX - BAR_SIZE) + accelX) && ((bar->coordX + BAR_SIZE) + accelX < SCREEN_X - WALL_WIDHT_X))
         bar->coordX += accelX;
     else {
         if (SCREEN_X / 2 >= bar->coordX) 
             bar->coordX -= bar->coordX - BAR_SIZE - WALL_WIDHT_X;
         else 
-            bar->coordX += -(bar->coordX + BAR_SIZE) + SCREEN_X - WALL_WIDHT_X;
+            bar->coordX += -(bar->coordX + BAR_SIZE) + SCREEN_X - WALL_WIDHT_X - 1;
     }
 }
