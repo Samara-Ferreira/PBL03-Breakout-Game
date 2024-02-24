baf6bce1d9f3361a30237e3a6eec7e08a645941d
Silviozv <109181824+Silviozv@users.noreply.github.com>
2024-02-24 15:36:55 -0300

    ✨ feat(constants): Colocando as cores básicas.

--- Modules/constants.h
+++ Modules/constants.h
@@ -15,4 +15,15 @@
 #define BLOCK_LENGTH            37 
 #define BLOCK_WIDHT             12
 
+#define COLOR_WHITE             0xFFFF
+#define COLOR_YELLOW            0xFFE0
+#define COLOR_RED               0xF800
+#define COLOR_GREEN             0x07E0
+#define COLOR_BLUE              0x041F
+#define COLOR_CYAN              0x07FF
+#define COLOR_MAGENTA           0xF81F
+#define COLOR_GREY              0xC618
+#define COLOR_PINK              0xFC18
+#define COLOR_ORANGE            0xFC00
+
 #endif // CONSTANTS_H_INCLUDED
